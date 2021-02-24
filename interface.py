68ca9afca7e6e925acb894c185d442091757fb3e
devinbrowndev1 <devinbrowndev@gmail.com>
2021-02-23 18:07:09 -0800

    shrug

--- interface.py
+++ interface.py
@@ -1,11 +1,33 @@
 from engine import Computer
 import chess
+import chess.svg
+import time
+import sys
+import webbrowser
 
-game = chess.Board()
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+board = chess.Board()
 opponent = Computer(10)
+opponent.print_board()
 
 if __name__ == "__main__":
-    while not game.is_checkmate():
+    while not board.is_checkmate():
+        chessboardSvg = chess.svg.board(board)
+        f1 = open('test.svg','w')
+        f1.write(chessboardSvg)
+        f1.close()
+
+        # MacOS
+        
+
+        webbrowser.get(chrome_path).open('test.svg')
+
+
+        
+
+
+
         print("Make a move:")
         move = input()
 
@@ -13,13 +35,15 @@ if __name__ == "__main__":
             break
 
         try:
-            move = game.push_san(move)
+            move = board.push_san(move)
             uci_move = move.uci()
         except:
             continue
 
         auto_move = opponent.get_move(uci_move)
-        game.push(chess.Move.from_uci(auto_move))
+        board.push(chess.Move.from_uci(auto_move))
 
         opponent.print_board()
+
+    
 
