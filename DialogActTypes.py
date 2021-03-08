d77212160ea48f2f3f896b56ea48d08c3a0759d0
Jessica Sweeney <jsweeney97@uchicago.edu>
2021-03-08 13:05:39 -0500

    nlg stuff

---
+++ DialogActTypes.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class DialogActTypes(Enum):
+    UNDEFINED = 1
+    REQUEST_BEST = 2
+    MOVE = 3
+
+
---
+++ NLG.py
@@ -0,0 +1,18 @@
+
+
+class NLG:
+    def __init__(self):
+        self.Name = "NLGDefault"
+
+
+    def generate(self, dialogAct, more_info):
+        # clarifying questions
+        #   if two pieces could make a move, specify which
+        #   if move wasn't heard correctly (or at all), ask to restate
+        #   if move is illegal, ask for new move
+
+        # responding with computer's move
+
+        # responding with best engine move
+        # asking if they want to play the best engine move
+
